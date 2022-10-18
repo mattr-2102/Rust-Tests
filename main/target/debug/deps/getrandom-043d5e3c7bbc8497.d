c3a3f04a92967e39a398b3b3d454e20d08afc376
mattr-2102 <matthew.rollason22@gmail.com>
2022-10-17 21:57:40 -0400

    adding more deps

---
+++ main/target/debug/deps/getrandom-043d5e3c7bbc8497.d
@@ -0,0 +1,9 @@
+c:\Users\drewr\OneDrive\Desktop\Projects\Personal\Rust\Rust-Tests\main\target\debug\deps\getrandom-043d5e3c7bbc8497.rmeta: C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\lib.rs C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\error.rs C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\util.rs C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\error_impls.rs C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\windows.rs
+
+c:\Users\drewr\OneDrive\Desktop\Projects\Personal\Rust\Rust-Tests\main\target\debug\deps\getrandom-043d5e3c7bbc8497.d: C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\lib.rs C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\error.rs C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\util.rs C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\error_impls.rs C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\windows.rs
+
+C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\lib.rs:
+C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\error.rs:
+C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\util.rs:
+C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\error_impls.rs:
+C:\Users\drewr\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.7\src\windows.rs:
---
+++ main/target/debug/deps/main-00a8e53605c3eb9a.d
@@ -0,0 +1,5 @@
+c:\Users\drewr\OneDrive\Desktop\Projects\Personal\Rust\Rust-Tests\main\target\debug\deps\main-00a8e53605c3eb9a.rmeta: src\main.rs
+
+c:\Users\drewr\OneDrive\Desktop\Projects\Personal\Rust\Rust-Tests\main\target\debug\deps\main-00a8e53605c3eb9a.d: src\main.rs
+
+src\main.rs:
